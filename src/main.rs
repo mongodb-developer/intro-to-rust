4f1b93ee53923bafafc7c2698d62cb369000a894
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-05-09 16:54:21 +0200

    Use String vs &str

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    let mut whom = "World";
+    let mut whom: &str = "World";
     println!("Hello, {whom}!");
     whom = "Planet";
     println!("Bye, {whom}!");
+
+    // Allocated in the stack
+    // let x = String::from("This is not a &str");
+    let x = "This is not a &str".to_string();
+    println!("x = {x}");
+    //     let y = x;
+    //     println!("x = {x}, y = {y}");
 }
