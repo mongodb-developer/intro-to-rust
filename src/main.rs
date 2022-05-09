a9991dcefb199f0f7f40e40be82e5b437663427e
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-05-09 17:40:01 +0200

    Use shadowing, partial type inference and print a result

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn main() {
     let x = String::from("This is not a &str");
     use_string(&x);
     println!("x = {x}");
+
+    let y = "5";
+    let y: Result<i32, _> = y.parse();
+    println!("y = {:?}", y);
 }
 
 fn use_string(s: &str) {
