9f079c792e14b36cd311f2351131f9ed3aa55071
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-05-10 15:51:12 +0200

    Define unit struct in module

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
+mod task;
+
+use task::Task;
+
 fn main() {
-    let mut whom: &str = "World";
-    println!("Hello, {whom}!");
-    whom = "Planet";
-    println!("Bye, {whom}!");
-
-    // Allocated in the stack
-    let x = String::from("This is not a &str");
-    use_string(&x);
-    println!("x = {x}");
-
-    let y = "5";
-    let y: Option<i32> = y.parse().ok();
-    if let Some(v) = y {
-        println!("y = {}", v);
-    }
-}
-
-fn use_string(s: &str) {
-    println!("Inside use_string(): s = {s}");
+    let task = Task {};
+    println!("Task: {task:?}");
 }
