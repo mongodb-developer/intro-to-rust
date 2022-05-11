079ddbd860d759e643031f274051ae33993487cc
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-05-11 10:40:12 +0200

    Define InMemoryRepo and add method to it

---
+++ src/in_memory_task_repo.rs
@@ -0,0 +1,16 @@
+use crate::task::Task;
+
+#[derive(Debug)]
+pub struct InMemoryTaskRepo {
+    tasks: Vec<Task>,
+}
+
+impl InMemoryTaskRepo {
+    pub fn new() -> InMemoryTaskRepo {
+	InMemoryTaskRepo { tasks: vec![] }
+    }
+
+    pub fn add(&mut self, task: Task) {
+	self.tasks.push(task);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 mod task;
+mod in_memory_task_repo;
 
+use chrono::Utc;
+use in_memory_task_repo::InMemoryTaskRepo;
 use task::Task;
 
 fn main() {
     let task = Task::new("Do homework".to_string(), None, false);
     println!("Task: {task:?}");
+
+    let mut repo = InMemoryTaskRepo::new();
+    repo.add(task);
+    repo.add(Task::new("Prepare dinner".to_string(), Some(Utc::today().naive_local()), false));
+
+    println!("Task repo: {repo:#?}");
 }
