d3d5b1c6e49e8d6d596e8b9648e838c2ce2b17a3
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-05-12 09:52:12 +0200

    Use concurrency to query Atlas cluster

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ mod task_repo;
 
 use chrono::Utc;
 use in_memory_task_repo::InMemoryTaskRepo;
+use mongodb::{options::{ClientOptions, ResolverConfig}, Client};
+use std::{env, error::Error};
 use task_repo::TaskRepo;
 use task::Task;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client_uri = env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+
     let task = Task::new("Do homework".to_string(), None, false);
     println!("Task: {task:?}");
 
@@ -17,4 +22,12 @@ fn main() {
 
     println!("Tasks in repo: {:#?}", repo.list());
     println!("Tasks in repo: {repo}");
+
+    let options = ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare()).await?;
+    let client = Client::with_options(options)?;
+    println!("Databases:");
+    for name in client.list_database_names(None, None).await? {
+	println!("- {}", name);
+    }
+    Ok(())
 }
